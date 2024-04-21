4d2e3a0006f2e108acd3adeeb2b1dc7dd43133d2
pranayshthakur <pranayshthakur@gmail.com>
2024-04-21 23:33:27 +0530

    dff

--- google1.py
+++ google1.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-# Test with Chrome
-driver = webdriver.Chrome()
+options = Options()
+##options.add_argument('--headless')  # Runs Chrome in headless mode
+options.add_argument('--no-sandbox')  # Bypass OS security model, required in some environments
+options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource problems
+options.add_argument('--disable-gpu')  # Applicable to headless chrome since Chrome 61
+
+driver = webdriver.Chrome(options=options)
+
 driver.get("https://www.google.com")
 driver.implicitly_wait(10)
 driver.quit()
